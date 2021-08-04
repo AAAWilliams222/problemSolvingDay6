020546b2cc985f986cb9b7fab778e552cb087032
Anthony D Williams <anthonywilliams222@gmail.com>
2021-08-04 09:19:12 -0500

    Final Commit

--- script6.js
+++ script6.js
@@ -5,51 +5,51 @@
 // a.	Write code that takes a string as input and returns the string reversed
 // b.	i.e. “Hello” will be returned as “olleH”
 
-let firstWord = prompt("Please type in a word.");
+// let firstWord = prompt("Please type in a word.");
 
-for(let i = firstWord.length - 1; i >= 0; i--){
-    console.log(firstWord[i]);
-}
+// for(let i = firstWord.length - 1; i >= 0; i--){
+//     console.log(firstWord[i]);
+// }
 
 // PROBLEM 2
 // Capitalize letter
 // a.	Write code that takes a string as input and capitalize the first letter of each word. 
 // Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World”
 
-let secondWord = prompt("Please type in another word.");
+// let secondWord = prompt("Please type in another word.");
 
-console.log(secondWord[0].toUpperCase() + secondWord.slice(1));
+// console.log(secondWord[0].toUpperCase() + secondWord.slice(1));
 
 // PROBLEM 3	
 // Compress a string of characters
 // a.	For example, an input of "aaabbbbbccccaacccbbbaaabbbaaa" would compress to "3a5b4c2a3c3b3a3b3a"
 
-    let stringCompression = (s) => {
-    // travers string, keep count of repeated chars
-    // if current and next char is the same, incr count
-    // otherwise, concat current char and count to output string, reset count to 1
-    //return compressed string, only if the length is less than the original string, otherwise, return original string 
-    let out = "";
-    let count = 1;
-    for(let i = 0; i < s.length; i++){
-        let current = s[i];
-        let next = s[i + 1];
-        if (current === next) {
-            count++;
-        } else {
-            out += current + String(count);
-            count = 1;
-        }
-    }
-    return out.length < s.length ? out : s;
-}
+//     let stringCompression = (s) => {
+//     // travers string, keep count of repeated chars
+//     // if current and next char is the same, incr count
+//     // otherwise, concat current char and count to output string, reset count to 1
+//     //return compressed string, only if the length is less than the original string, otherwise, return original string 
+//     let out = "";
+//     let count = 1;
+//     for(let i = 0; i < s.length; i++){
+//         let current = s[i];
+//         let next = s[i + 1];
+//         if (current === next) {
+//             count++;
+//         } else {
+//             out += current + String(count);
+//             count = 1;
+//         }
+//     }
+//     return out.length < s.length ? out : s;
+// }
 
-console.log(
-    stringCompression("aabcccccaaa") === "a2b1c5a3",
-    stringCompression("aa") === "aa",
-    stringCompression("aaAAaa") === "aaAAaa",
-    stringCompression("aaaAAaa") === "a3A2a2",
-);
+// console.log(
+//     stringCompression("aabcccccaaa") === "a2b1c5a3",
+//     stringCompression("aa") === "aa",
+//     stringCompression("aaAAaa") === "aaAAaa",
+//     stringCompression("aaaAAaa") === "a3A2a2",
+// );
 
 
 // PROBLEM 4
@@ -59,16 +59,16 @@ console.log(
 
 
 
-let str = prompt("Enter Palindrome.");
-let isPalindrome = (inStr) => {
-    for(let i = 0; i < inStr.length; i++){
-        if(inStr[i] !== inStr[inStr.length - 1 - i]) {
-            return false;
-        }
-    }
-    return true;
-}
-console.log(isPalindrome(str));
+// let str = prompt("Enter Palindrome.");
+// let isPalindrome = (inStr) => {
+//     for(let i = 0; i < inStr.length; i++){
+//         if(inStr[i] !== inStr[inStr.length - 1 - i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// console.log(isPalindrome(str));
 
 
 // PROBLEM 1
@@ -80,7 +80,25 @@ console.log(isPalindrome(str));
 // c.	Write a method that determines if a number is happy or sad
 
 
+// function happyNumber(num){
+//     let previouslySeenSums = new Set();
+//     while(num !== 1){
+//         let currentSum = 0;
+//         while(num > 0){
+//             currentSum += (num % 10) * (num %10)
+//             num = Math.floor(num / 10)
+//         }
+//         if(!previouslySeenSums.has(currentSum)){
+//             previouslySeenSums.add(currentSum)
+//             num = currentSum
+//         } else {
+//             return false
+//         }
+//     }
+//     return true
+// }
 
+// console.log(happyNumber(19));
 
 
 // PROBLEM 2
@@ -88,7 +106,10 @@ console.log(isPalindrome(str));
 // a.	A prime number is a number that is only divisible by one and itself.
 // b.	Write a method that prints out all prime numbers between 1 and 100 
 
+for( let i = 1; i <= 100; i++){
 
+    console.log(i % 10);
+}
 
 
 
@@ -98,3 +119,20 @@ console.log(isPalindrome(str));
 //     The simplest is the series 1, 1, 2, 3, 5, 8, etc.
 // b.	Write a method that does the Fibonacci sequence starting at 1
 // c.	HARDER VERSION: Write a method that does the Fibonacci sequence starting at a number that a user inputs
+
+// 0, 1
+// add numberOne and numberTwo, get numberThree
+// add numberTwo and numberThree, get numberFour
+// add numberThree and numberFour, get numberFive
+// for loop - start with 20 loops
+// new variable to hold result - array?
+let fibonacciArray = []
+let numberOne = 0;
+let numberTwo = 1;
+let numberThree;
+
+for (let i = 0; i < 20; i++) {
+  numberThree = numberOne + numberTwo; 
+    
+}
+console.log(fibonacciArray)//0, 1, 1, 2, 3, 5, 8, etc
